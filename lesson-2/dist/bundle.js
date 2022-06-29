553eddcd7abf7708077d40a6d2cda156578408be
Anderson <andersoncsilva850@gmail.com>
2022-06-29 11:12:47 -0300

    primeiro contanto

---
+++ lesson-2/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ../node_modules/babel-loader/lib/index.js):\\nError: Unknown option: .present. Check out https://babeljs.io/docs/en/babel-core/#options for more information about options.\\n    at throwUnknownError (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\options.js:134:27)\\n    at C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\options.js:119:5\\n    at Array.forEach (<anonymous>)\\n    at validateNested (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\options.js:95:21)\\n    at validate (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\options.js:86:10)\\n    at C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:209:34\\n    at cachedFunction (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\caching.js:60:27)\\n    at cachedFunction.next (<anonymous>)\\n    at evaluateSync (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\gensync\\\\index.js:251:28)\\n    at sync (C:\\\\Users\\\\Anderson_PC\\\\Desktop\\\\Spread Fullstack Developer\\\\apredendo-ReactJS\\\\node_modules\\\\gensync\\\\index.js:89:14)\");\n\n//# sourceURL=webpack://lesson-2/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
